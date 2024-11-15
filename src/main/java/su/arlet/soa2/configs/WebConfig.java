fad80ea411e927d9aef630fdaa4d435c3b2acc19
arlet <shulga@niuitmo.ru>
2024-11-15 22:19:11 +0300

    add cors

---
+++ src/main/java/su/arlet/soa2/configs/WebConfig.java
@@ -0,0 +1,18 @@
+package su.arlet.soa2.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST", "GET", "PUT", "DELETE")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
