3ab269a0cb1cbe6f12619c03ac7ce09b6c245a56
arlet <shulga@niuitmo.ru>
2025-01-17 00:42:56 +0300

    fixes

--- service2/src/main/java/su/arlet/controller/StarshipController.java
+++ service2/src/main/java/su/arlet/controller/StarshipController.java
@@ -5,29 +5,32 @@ import su.arlet.ejb.StarshipServiceRemote;
 
 import javax.ejb.EJB;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@WebService
+@WebService(targetNamespace = "http://example.com/webservices")
 public class StarshipController {
 
     @EJB(lookup = "ejb:/ejb-module-0.0.1-SNAPSHOT/StarshipServiceBean!su.arlet.ejb.StarshipServiceRemote")
     private StarshipServiceRemote starshipService;
 
     @WebMethod
-    public Response unloadSpaceMarine(@PathParam("starship-id") long starshipId, @PathParam("space-marine-id") long spaceMarineId) {
+    public Response unloadSpaceMarine(
+            @WebParam(name = "starship-id") long starshipId,
+            @WebParam(name = "space-marine-id") long spaceMarineId
+    ) {
         return Response.status(starshipService.unloadSpaceMarine(starshipId, spaceMarineId)).build();
     }
 
     @WebMethod
-    public Response unloadAll(@PathParam("starship-id") long starshipId) {
-            return Response.status(starshipService.undeployAll(starshipId)).build();
+    public Response unloadAll(@WebParam(name="starship-id") long starshipId) {
+        return Response.status(starshipService.undeployAll(starshipId)).build();
     }
 
     @WebMethod
-    public Response createStarship(StarshipCreator starship) {
+    public Response createStarship(@WebParam(name="starship") StarshipCreator starship) {
         return Response.status(starshipService.createStarship(starship)).build();
     }
 }
